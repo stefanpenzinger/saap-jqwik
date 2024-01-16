7441dffd8ad2c8156b50d490e91f3de2c96d0caf
Stefan Penzinger <stefan.penzinger13@gmail.com>
2024-01-16 19:00:01 +0100

    Extend property test with provider

--- src/test/java/at/fh/hagenberg/BubbleSortPropertyTest.java
+++ src/test/java/at/fh/hagenberg/BubbleSortPropertyTest.java
@@ -2,11 +2,15 @@ package at.fh.hagenberg;
 
 import net.jqwik.api.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class BubbleSortPropertyTest {
     private final BubbleSort bubbleSort = new BubbleSort();
 
     @Property
-    boolean array_is_sorted(@ForAll int[] integerArray) {
+    boolean arrayIsSorted(@ForAll int[] integerArray) {
         boolean isSorted = true;
         bubbleSort.bubbleSort(integerArray);
 
@@ -22,5 +26,45 @@ public class BubbleSortPropertyTest {
         }
 
         return isSorted;
+    }
+
+    @Property
+    boolean arrayIsSortedDomainSpecific(@ForAll("integersNotBetween1112And2221") Integer[] integerArray) {
+        boolean isSorted = true;
+        int arraySize = integerArray.length;
+        int[] arr = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            arr[i] = integerArray[i];
+        }
+
+        bubbleSort.bubbleSort(arr);
+
+        if (integerArray.length == 0 || integerArray.length == 1) {
+            return isSorted;
+        }
+
+        for (int i = 1; i < integerArray.length; i++) {
+            if (integerArray[i - 1] > integerArray[i]) {
+                isSorted = false;
+                return isSorted;
+            }
+        }
+
+        return isSorted;
+    }
+
+    @Provide
+    public Arbitrary<Integer[]> integersNotBetween1112And2221() {
+        return Arbitraries.integers()
+                .between(0, 100)
+                .array(Integer[].class)
+                .ofMinSize(0)
+                .ofMaxSize(30)
+                .map(anArray -> {
+                    Arrays.sort(anArray);
+                    return Arrays.stream(anArray)
+                            .filter(value -> value < 1112 || value > 2221)
+                            .toArray(Integer[]::new);
+                });
     }
 }
